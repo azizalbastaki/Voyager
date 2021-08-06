c41446d23f913604cfafc947df5258f029065eb4
Abdulaziz <azalbastaki@live.com>
2021-08-06 04:33:02 +0400

    GUI

--- objectLib.py
+++ objectLib.py
@@ -7,10 +7,6 @@ class dock():
         self.port.setPos(location)
         self.port.setH(90)
         self.port.setCollideMask(BitMask32.bit(1))
-        #ambiet = AmbientLight('ambient')
-        #ambiet.setColor((0.2, 0.2, 0.2, 1))
-        #alight = self.port.attachNewNode(ambiet)
-        #self.port.setLight(alight)
         self.port.setShaderAuto()
         self.port.reparentTo(render)
 class collisions():
